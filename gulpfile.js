d3770bc2eaa3560df8718dfb134b7fec1f3f0b6f
arturoguedez <arturo.guedez@gmail.com>
2018-04-22 22:02:39 -0400

    Convert to typescript (#2)
    * first pass at typescript
    
    * testing up watch
    
    * removing the dist folder
    
    * adding dist to ignore
    
    * adding code coverage
    
    * removing unused package

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,17 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
+const ts = require('gulp-typescript');
+const nodemon = require('gulp-nodemon');
+const run = require('gulp-run-command').default;
+
+const tsProject = ts.createProject('tsconfig.json', {
+    declaration: true
+});
+
+const tsTestProject = ts.createProject('tsconfig.json', {
+    declaration: true
+});
 
 // fetch command line arguments
 const arg = ((argList) => {
@@ -32,7 +43,7 @@ gulp.task('lint', () => {
     // So, it's best to have gulp ignore the directory as well.
     // Also, Be sure to return the stream from the task;
     // Otherwise, the task may end before the stream has finished.
-    return gulp.src(['**/*.js', '!node_modules/**'])
+    return gulp.src(['src/**/*.ts', '!node_modules/**'])
         // eslint() attaches the lint output to the "eslint" property
         // of the file object so it can be used by other modules.
         .pipe(eslint())
@@ -44,18 +55,50 @@ gulp.task('lint', () => {
         .pipe(eslint.failAfterError());
 });
 
-gulp.task('test', () => {
-    let defaultTests = ['tests/**/*.spec.js'];
-    let tests = defaultTests;
+// Runs all the test, doing validation for code coverage.
+gulp.task('code-coverage', ['test-scripts'], run("nyc --check-coverage --lines 95 --functions 95 --branches 95 mocha --require co-mocha 'dist-test/**/*.spec.js'"));
+
+// Runs all the tests, or a single one (if --spec is passed in) without code coverage
+gulp.task('test', ['test-scripts'],  () => {
+    let defaultTests = ['dist-test/**/*.spec.js'];
+    let tests;
+
     if (arg.spec) {
-        tests = ['tests/' + arg.spec + '.spec.js'];
+        tests = ['dist-test/' + arg.spec + '.spec.js'];
+    } else {
+        tests = defaultTests;
     }
-    gulp.src(tests, {read: false})
-        .pipe(mocha({reporter: 'list', exit: true}))
-        .on('error', console.error);
+
+    return gulp.src(tests, {read: false})
+            .pipe(mocha({reporter: 'list', exit: true}))
+            .on('error', console.error);
 });
 
-gulp.task('default', ['lint', 'test'], function () {
+gulp.task('default', ['lint', 'scripts', 'test'], () => {
     // This will only run if the lint task is successful...
 });
 
+gulp.task('test-scripts', () => {
+    return gulp.src(['src/**/*.ts'])
+        .pipe(tsTestProject())
+        .pipe(gulp.dest('dist-test'));
+});
+
+gulp.task('scripts', () => {
+    return gulp.src(['src/**/*.ts','!src/**/*.spec.ts'])
+        .pipe(tsProject())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', ['scripts'], () => {
+    return gulp.watch(['src/**/*.ts', '!src/**/*.spec.js'], ['scripts']);
+});
+
+gulp.task('dev', ['scripts', 'watch'], () => {
+    return nodemon({
+            script: 'dist/index.js',
+            watch: ['dist'],
+            ignore: ['dist/**/*.spec.js', 'dist/**/*.d.ts'],
+            env: { 'NODE_ENV': 'development' }
+        });
+})
--- src/helpers/first.spec.ts
+++ src/helpers/first.spec.ts
@@ -1,9 +1,10 @@
 'use strict';
 const expect = require('chai').expect;
-const first = require('../../helpers/first');
+const first = require('./first');
 
 describe('first test', function () {
     it('test the addition', function () {
         expect(first.add(1, 2)).to.eql(3);
+        expect(3).to.eql(3);
     });
 });
