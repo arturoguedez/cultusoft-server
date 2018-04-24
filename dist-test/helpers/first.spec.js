a1d2006059fde7d1e24a8ddfeba5314970557624
arturoguedez <arturo.guedez@gmail.com>
2018-04-23 21:35:41 -0400

    deleting the dist folder (#5)

--- dist-test/helpers/first.spec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const expect = require('chai').expect;
-const first = require('./first');
-describe('first test', function () {
-    it('test the addition', function () {
-        expect(first.add(1, 2)).to.eql(3);
-        expect(3).to.eql(3);
-    });
-});
